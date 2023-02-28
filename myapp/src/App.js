306f9b2a434f4b1c4e4653a09c18278926fbea85
jm181987 <jorgitom18@gmail.com>
2023-02-28 14:54:04 +0000

    ok

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './assets/logo.svg';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hola mundo</p>
+    </div>
+  );
+}
+
+export default App;
